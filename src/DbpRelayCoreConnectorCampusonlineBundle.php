e4100ebcb938ef33d90784fdfd118b4b8a12a492
Christoph Reiter <reiter.christoph@gmail.com>
2022-11-23 13:16:36 +0100

    Initial commit

---
+++ src/DbpRelayCoreConnectorCampusonlineBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\CoreConnectorCampusonlineBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DbpRelayCoreConnectorCampusonlineBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+    }
+}
